49dafdd120bb641830f3d2ea4ed75fc972cb7553
Alondra <asmayen@uamv.edu.ni>
2025-06-06 14:30:43 -0600

    classes.py

--- inventario_ordex/models/classes.py
+++ inventario_ordex/models/classes.py
@@ -1,8 +1,8 @@
 class Product:
-    def __init__(self, nombre, precio, existencia):  # Esta parte se ejecuta cuando se crea un nuevo producto
-        self.nombre = nombre  # Guarda el nombre del producto
-        self.precio = precio   # Guarda el precio del producto
-        self.existencia = existencia  # Guarda cuántas unidades hay disponibles
+    def __init__(self, nombre, precio, existencia):  # Esta parte se ejecuta cuando se crea un nuevo producto.
+        self.nombre = nombre  # Guarda el nombre del producto.
+        self.precio = precio   # Guarda el precio del producto.
+        self.existencia = existencia  # Guarda cuántas unidades hay disponibles.
 
-    def __str__(self):  # Define cómo se mostrará el producto cuando lo imprimimos
+    def __str__(self):  # Define cómo se mostrará el producto cuando lo imprimimos.
         return f"{self.nombre} | Precio: ${self.precio:.2f} | Existencia: {self.existencia}"
