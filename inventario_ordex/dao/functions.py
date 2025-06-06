f6a909b92e6a2f53c26fef5b215b61b5fc691fc2
AlondraMayen <asmayen@uamv.edu.ni>
2025-06-06 13:25:27 -0600

    Data Access Object

---
+++ inventario_ordex/dao/functions.py
@@ -0,0 +1,14 @@
+class productDao:
+    def __init__(self):
+        self.productos = [] # Se crea una lista vacía para guardar los productos
+
+    def add(self, producto): # Se agrega un producto a la lista
+        self.productos.append(producto)
+        print("Producto agregado.")
+
+    def show(self): # Muestra todos los productos registrados
+        if not self.productos:
+            print("No hay productos registrados.")
+        else:
+            for i, producto in enumerate(self.productos, 1):
+                print(f"{i}. {producto}")
---
+++ inventario_ordex/main/menu.py
@@ -0,0 +1,40 @@
+import models.classes as c
+import dao.functions as f
+
+productos = f.productDao() # Crea un objeto que se encargará de guardar y mostrar productos
+
+def menu(): # Muestra el menú de opciones
+    print("""
+1. Agregar producto
+2. Mostrar productos
+6. Salir
+""")
+
+def main():
+    while True: # Bucle principal del programa
+        menu() # Muestra el menú
+        option = input("Digite una opción: ") # Pide la opción al usuario
+
+        if option == '1':  # Agregar producto
+            nombre = input("Nombre del producto: ")
+            try:
+                precio = float(input("Precio: ")) 
+                existencia = int(input("Existencia: "))
+            except ValueError: # Vuelve al menú si hay error
+                print("Error: precio debe ser decimal y existencia un número entero.")
+                continue
+            
+            # Crea un nuevo producto con los datos ingresados   
+            producto = c.Product(nombre, precio, existencia)
+            productos.add(producto)
+
+        elif option == '2':
+            print("\n--- Lista de Productos ---")
+            productos.show() # Mostrar todos los productos
+ 
+        elif option == '6': # Salir del programa
+            print("¡Adiós!")
+            break
+
+        else: # Opción inválida
+            print("Opción no válida, intente de nuevo.")
